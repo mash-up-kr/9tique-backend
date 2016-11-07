9e7aa5d53d65185d15a0c352ac2d14436c626a42
DongHee Kim <opklnm102@gmail.com>
2016-11-08 01:41:00 +0900

    Create spring boot project

---
+++ src/main/java/kr/co/mash_up/NineTiqueApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mash_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineTiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NineTiqueApplication.class, args);
+        System.out.println("qtique Server Run!!");
+    }
+}
